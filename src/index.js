026271ae5dd1a1410037c98890cdf73167fdc18e
DeadBryam <Bryamu.u@gmail.com>
2019-12-22 13:29:21 -0600

    se agrego script al package.json para compilar

--- src/index.js
+++ src/index.js
@@ -1,20 +1,135 @@
-const {app, BrowserWindow, Menu} = require('electron');
-const url = require('url');
-const path = require('path');
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const url = require("url");
+const path = require("path");
 
-
-if(process.env.NODE_ENV != 'production'){
-    require('electron-reload')(__dirname,{});
+if (process.env.NODE_ENV != "production") {
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "../node_modules", ".bin", "electron")
+  });
 }
 
-let main;
-app.on('ready',()=> {
-    main = new BrowserWindow({});
-    main.loadURL(url.format({
-        pathname: path.join(__dirname, 'views/index.html'),
-        protocol: 'file',
-        slashes: true
-    }));
+let mainWindow;
+let newProductWindow;
 
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/index.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
 
+  const mainMenu = Menu.buildFromTemplate(templateMenu);
+
+  mainMenu.on("close", () => {
+    console.log("c cerro");
+    app.quit();
+  });
+
+  mainWindow.setMenu(mainMenu);
 });
+
+ipcMain.on("new-product", (evt, msg) => {
+  mainWindow.webContents.send("new-product", msg);
+  newProductWindow.close();
+});
+
+function createNewProductWindow() {
+  newProductWindow = new BrowserWindow({
+    width: 400,
+    height: 250,
+    title: "Agregar nuevo producto",
+    modal: true,
+    parent: mainWindow,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  newProductWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "views/new-product.html"),
+      protocol: "file",
+      slashes: true
+    })
+  );
+
+  const menu = Menu.buildFromTemplate(newProductMenu);
+  newProductWindow.setMenu(menu);
+  // newProductWindow.setMenuBarVisibility(false);
+
+  newProductWindow.on("close", () => {
+    newProductWindow = null;
+  });
+}
+
+const templateMenu = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Nuevo Producto",
+        accelerator: "Ctrl + P",
+        click: () => {
+          createNewProductWindow();
+        }
+      },
+      {
+        label: "Remove all",
+        click: ()=>{
+          mainWindow.webContents.send('remove-all');
+        }
+      },
+      {
+        label: "Exit",
+        accelerator: process.platform == "darwin" ? "command+w" : "Ctrl+w",
+        click: () => {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+const newProductMenu = [
+  {
+    label: "Close",
+    accelerator: "Ctrl+W",
+    click: () => {
+      newProductWindow.close();
+    }
+  }
+];
+
+if (process.platform === "darwin") {
+  templateMenu.unshift({
+    label: app.getName()
+  });
+}
+
+if (process.env.NODE_ENV !== "production") {
+  let DevTool = {
+    label: "DevMenu",
+    submenu: [
+      {
+        label: "Show/Hide DevTools",
+        accelerator: "F12",
+        click: (item, focusedWindow) => {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  };
+
+  templateMenu.push(DevTool);
+  newProductMenu.push(DevTool);
+}
